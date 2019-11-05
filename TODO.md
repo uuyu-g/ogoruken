0b094b60fb713b4fda5294f9a37b02eb82b4801d
yutaro <aqueous.mkli@gmail.com>
2019-11-05 23:29:53 +0900

    ogoriken → ogoruken

--- TODO.md
+++ TODO.md
@@ -3,9 +3,9 @@
 - [x] 概要の作成
 - [ ] 画面の列挙
 - [ ] 画面ごとのユースケースの列挙
-- [ ] Githubでリポジトリを作成
+- [x] Githubでリポジトリを作成
 - [ ] 実装
-- [ ] firebaseの設定
+- [x] firebaseの設定
 - [ ] Github Actionsの調査
 - [ ] Github Actionsの設定
 - [ ] TSlintからESlintに変更
@@ -17,7 +17,7 @@ NEM
 - [ ] favicon
 
 github
-- [ ] Github Projectの作成
+- [x] Github Projectの作成
 - [ ] tagでデプロイ
 
 firebase
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ogoriken'`, () => {
+  it(`should have as title 'ogoruken'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ogoriken');
+    expect(app.title).toEqual('ogoruken');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ogoriken app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('ogoruken app is running!');
   });
 });
